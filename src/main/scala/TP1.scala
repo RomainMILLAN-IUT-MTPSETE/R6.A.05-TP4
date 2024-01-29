ab9f2523ca8c2350fe102cdff7e3ccfeeaf5bc62
Simon Robillard <simon.robillard@umontpellier.fr>
2024-01-29 11:38:40 +0100

    TP1

---
+++ src/main/scala/TP1.scala
@@ -0,0 +1,156 @@
+import scala.math
+
+object TP1Ex1:
+
+  /* Ce trait définit le type de données abstrait d'une queue (first-in-first-out) */
+  trait Queue[A]:
+    def enqueue(a: A): Queue[A]
+    def dequeue(): Queue[A]
+    def peek(): A
+    def isEmpty(): Boolean
+
+  /* Donnez une définition concrète à ce type de données. Quelle est la complexité des différentes opérations? */
+  class ListQueue[A](l: List[A] = Nil) extends Queue[A]:
+
+    override def enqueue(a: A): Queue[A] = ???
+
+    override def dequeue(): Queue[A] = ???
+
+    override def peek(): A = ???
+
+    override def isEmpty(): Boolean = ???
+
+  /* Afin d'améliorer la complexité, on peut utiliser une implémentation basée sur deux listes:
+   * - front contient les premiers éléments ajoutés, de telle manière que front.head est le plus ancien élément de la
+   *   queue
+   * - rear contient les derniers éléments ajoutés, de telle manière que rear.head est le plus récent élément ajouté
+   *   (l'ordre des élément est donc l'inverse de celui de front)
+   *
+   * Implémentez les différentes opérations, de manière à maintenir ces invariants.
+   * Note: on peut aussi ajouter l'invariant suivant (pas obligatoire, mais il permet de simplifier certains cas)
+   * - front n'est vide que si toute la queue est vide (c'est-à-dire si rear l'est aussi)
+   */
+  class DoubleListQueue[A](front: List[A] = Nil, rear: List[A] = Nil) extends Queue[A]:
+
+    override def enqueue(a: A): Queue[A] = ???
+
+    override def dequeue(): Queue[A] = ???
+
+    override def peek(): A = ???
+
+    override def isEmpty(): Boolean = ???
+
+object TP1Ex2:
+
+  /* Ce trait définit le type de données abstrait d'un ensemble. */
+  trait Set[A]:
+    def elem(a: A): Boolean
+    def add(a: A): Set[A]
+    def remove(a: A): Set[A]
+
+  /* Donnez une définition concrète à ce type, basée sur une liste. */
+  class ListSet[A](l: List[A] = Nil) extends Set[A]:
+
+    override def elem(a: A): Boolean = ???
+
+    override def add(a: A): Set[A] = ???
+
+    override def remove(a: A): Set[A] = ???
+
+  /* Nous allons définir une implémentation plus efficace spécialement pour les types qui héritent du trait Ordered
+   * (sur lesquels on peut utiliser les comparaisons <, <=, >= et >) basées sur un arbre binaire de recherche.
+   * Pour cela on commence par définir un type de données algébrique pour les arbres binaires, et les opérations
+   * associées.
+   * Rappel: un arbre t est un arbre binaire de recherche si:
+   * - t un arbre vide Leaf(), ou
+   * - t est une Branch(x, l, r) qui satisfait les 3 conditions suivantes:
+   *   (1) toutes les valeurs dans l'arbre l sont strictement inférieures à x
+   *   (2) toutes les valeurs dans l'arbre r sont strictement supérieures à x
+   *   (3) l et r sont des arbres binaires de recherche
+   */
+  enum BinaryTree[A <: Ordered[A]]:
+    case Leaf()
+    case Branch(x: A, left: BinaryTree[A], right: BinaryTree[A])
+
+  def elemBST[A <: Ordered[A]](t: BinaryTree[A], a: A): Boolean = ???
+
+  def addBST[A <: Ordered[A]](t: BinaryTree[A], a: A): BinaryTree[A] = ???
+
+  def removeBST[A <: Ordered[A]](t: BinaryTree[A], a: A): BinaryTree[A] = ???
+
+  /* Utilisez le type de données BinaryTree pour implémenter la classe suivante qui implémente Set. */
+  class BstSet[A <: Ordered[A]](t: BinaryTree[A] = BinaryTree.Leaf[A]()) extends Set[A]:
+
+    override def elem(a: A): Boolean = ???
+
+    override def add(a: A): Set[A] = ???
+
+    override def remove(a: A): Set[A] = ???
+
+
+object TP1Ex3:
+
+  /* Définissez la classe Fraction qui implémente Ordered. */
+  class Fraction(val num: Int, val denum: Int) extends Ordered[Fraction]:
+
+    def negate(): Fraction = Fraction(-num, denum)
+
+    def invert(): Fraction = Fraction(denum, num)
+
+    def add(that: Fraction): Fraction = Fraction(this.num * that.denum + that.num * this.denum, this.denum * that.denum)
+
+    def sub(that: Fraction): Fraction = add(that.negate())
+
+    def mult(that: Fraction): Fraction = Fraction(this.num * that.denum * that.num * this.denum, this.denum * that.denum)
+
+    def div(that: Fraction): Fraction = mult(that.invert())
+
+    override def compare(that: Fraction): Int = this.num * that.denum - that.num * this.denum
+
+    /* compare doit être compatible avec l'égalité: this.compare(that) retourne 0 si et seulement si this == that */
+    override def equals(obj: Any): Boolean = obj match
+      case that: Fraction => this.num * that.denum == that.num * this.denum
+      case _ => false
+
+    /* deux objets égaux doivent avoir le même hashCode */
+    override def hashCode(): Int =
+      def gcd(x: Int, y: Int): Int = y match
+        case 0 => x
+        case _ => if y > y then gcd(x, y) else gcd(y, x % y)
+      { val d = gcd(num, denum) ; ((num / d) * (denum / d)).hashCode() }
+
+  class FractionIsFractional extends Fractional[Fraction]:
+
+    override def div(x: Fraction, y: Fraction): Fraction = x.div(y)
+
+    override def compare(x: Fraction, y: Fraction): Int = x.compare(y)
+
+    override def fromInt(x: Int): Fraction = Fraction(x, 1)
+
+    override def minus(x: Fraction, y: Fraction): Fraction = x.sub(y)
+
+    override def plus(x: Fraction, y: Fraction): Fraction = x.add(y)
+
+    override def times(x: Fraction, y: Fraction): Fraction = x.mult(y)
+
+    override def parseString(str: String): Option[Fraction] = ???
+
+    override def negate(x: Fraction): Fraction = x.negate()
+
+    override def toInt(x: Fraction): Int = x.num / x.denum
+
+    override def toDouble(x: Fraction): Double = x.num.toDouble / x.denum.toDouble
+
+    override def toFloat(x: Fraction): Float = x.num.toFloat / x.denum.toFloat
+
+    override def toLong(x: Fraction): Long = x.num.toLong / x.denum.toLong
+
+  enum FractionalFunction[T]:
+    case Const(v: T)
+    case Plus(f1: FractionalFunction[T], f2: FractionalFunction[T])
+
+  def eval[T](f: FractionalFunction[T])(implicit fr: Fractional[T]): T = f match
+    case FractionalFunction.Const(v) => v
+    case FractionalFunction.Plus(f1, f2) => fr.plus(eval(f1), eval(f2))
+
+
